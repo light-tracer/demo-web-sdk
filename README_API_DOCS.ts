f95ed70a72185f08b46902d20c58e1ab0aeab55f
duv <duv@email.com>
2023-10-12 17:09:39 +0300

    LTR demo JS SDK 2.7.0.98

--- README_API_DOCS.ts
+++ README_API_DOCS.ts
@@ -1,184 +1,274 @@
 class ErrorType {
-	SUCCESS,
-	INTERRUPT,
-	NO_DATA,
-	ERROR,
-	ERROR_IO,
-	ERROR_HANDLE_INVALID,
-	ERROR_NO_MEMORY,
-	ERROR_PROPERTY_NOT_FOUND,
+	SUCCESS : number = 0;
+	INTERRUPT : number = 1;
+	NO_DATA : number = 2;
+	ERROR : number = 3;
+	ERROR_IO : number = 4;
+	ERROR_HANDLE_INVALID : number = 5;
+	ERROR_NO_MEMORY : number = 6;
+	ERROR_PROPERTY_NOT_FOUND : number = 7;
 }
 
 class VisibilityMode {
-	HIDDEN,
-	CAMERA_HIDDEN,
-	FULLY_VISIBLE,
+	HIDDEN : number = 0;
+	CAMERA_HIDDEN : number = 1;
+	FULLY_VISIBLE : number = 2;
 }
 
 class MaterialPropertyColor {
-	BASE_COLOR,
-	COATING_COLOR,
-	EMISSION_COLOR,
-	ABSORPTION_COLOR,
-	GLASS_COLOR,
-}
-
-class MaterialPropertyColor {
-	BASE_COLOR,       // Base color of the surface
-	COATING_COLOR,    // Absorption of the coating
-	EMISSION_COLOR,   // Color of the emitted radiance
-	ABSORPTION_COLOR, // Residual absorption color
-	GLASS_COLOR       // Color to modulate reflection/refraction
+	BASE_COLOR : number = 0;       // Base color of the surface
+	COATING_COLOR : number = 1;    // Absorption of the coating
+	EMISSION_COLOR : number = 2;   // Color of the emitted radiance
+	ABSORPTION_COLOR : number = 3; // Residual absorption color
+	GLASS_COLOR : number = 4;      // Color to modulate reflection/refraction
 }
 
 class MaterialProperty {
   //! Surface metalness. Metallic model has no diffuse
   //! component and also has tinted specular highlight.
-  METALNESS,
+  METALNESS : number = 0;
 
   //! Surface roughness that controls both diffuse and
   //! specular response.
-  ROUGHNESS,
+  ROUGHNESS : number = 1;
 
   //! Normalized refractive index of smooth dielectric
   //! coating layer. Use 0 for disabling coating layer,
   //! and 1 for highly reflective coating.
-  CLEARCOAT,
+  CLEARCOAT : number = 2;
 
   //! Thickness of the coating layer used for modeling
   //! absorption. Set to 0 for non-absorbing and 5 for
   //! highly absorbing coating.
-  THICKNESS,
+  THICKNESS : number = 3;
 
   //! Degree of anisotropy controlling an aspect ratio
   //! of specular highlight. Use 0 for isotropic model,
   //! and 1 for maximally anisotropic.
-  ANISOTROPY,
+  ANISOTROPY : number = 4;
 
   //! Adjusts base diffuse shape with Hanrahan-Krueger
   //! subsurface approximation.
-  FLATNESS,
+  FLATNESS : number = 5;
 
   //! Additional grazing component, primarily intended
   //! for cloth rendering.
-  SHEEN,
+  SHEEN : number = 6;
 
   //! Opacity of the base surface layer.
-  OPACITY,
+  OPACITY : number = 7;
 
   //! Adjusts specular reflection at normal incidence.
   //! Used instead of an explicit index of refraction.
-  SPECULAR,
+  SPECULAR : number = 8;
 
   //! Tints the sheen towards the base color.
-  SHEEN_TINT,
+  SHEEN_TINT : number = 9;
 
   //! Tints light transmission towards the base color.
-  OPACITY_TINT,
+  OPACITY_TINT : number = 10;
 
   //! Artistic control to tint the specular highlight
   //! towards the base color. The grazing specular is
   //! still achromatic.
-  SPECULAR_TINT,
+  SPECULAR_TINT : number = 11;
 
   /////////////////////////////////////////////////////////
   // GLass params
 
   //! Refractive index (Cauchy-A coefficient).
-  GLASS_IOR,
+  GLASS_IOR : number = 12;
 
   //! Dispersion value (Cauchy-B coefficient).
-  GLASS_DISPERSION,
+  GLASS_DISPERSION : number = 13;
 
   //! Beckmann roughness of the unresolved surface.
   //! Root mean square (RMS) slope of micro-facets.
-  GLASS_ROUGHNESS,
+  GLASS_ROUGHNESS : number = 14;
 
   //! Tints the reflection towards the base color.
-  GLASS_REFLECT_TINT,
+  GLASS_REFLECT_TINT : number = 15;
 
   //! Tints the refraction towards the base color.
-  GLASS_REFRACT_TINT,
+  GLASS_REFRACT_TINT : number = 16;
 
   //! Linear attenuation factor
-  ABSORPTION_COEFF,
+  ABSORPTION_COEFF : number = 17;
 }
 
 class TextureRole
 {
-  BASE_NORMAL_TEXTURE,
-  BASE_COLOR_TEXTURE,
-  METALNESS_TEXTURE,
-  ROUGHNESS_TEXTURE,
-  EMISSION_TEXTURE,
-  COAT_NORMAL_TEXTURE,
-  EXT_OPACITY_TEXTURE,
-  IES_PROFILE_TEXTURE,
-  LIGHT_SHAPE_TEXTURE,
+  BASE_NORMAL_TEXTURE : number = 0;
+  BASE_COLOR_TEXTURE : number = 1;
+  METALNESS_TEXTURE : number = 2;
+  ROUGHNESS_TEXTURE : number = 3;
+  EMISSION_TEXTURE : number = 4;
+  COAT_NORMAL_TEXTURE : number = 5;
+  EXT_OPACITY_TEXTURE : number = 6;
+  IES_PROFILE_TEXTURE : number = 7;
+  LIGHT_SHAPE_TEXTURE : number = 8;
 }
 
-//! Sets named parameter for the renderer.
-SetParam: (context: ContextType, name: string, value: number) => ErrorType
+class ContextType {
+  id : number;
+}
+class NodeType {
+  id : number;
+}
+class TextureType {
+  id : number;
+}
+class ClipType {
+  id : number;
+}
+class LightEnvType {
+  id : number;
+}
 
-//! Returns named parameter of the renderer.
-GetParam: (context: ContextType, name: string) => number
+declare class ApiObject {
 
-// List of possible parameters:
+  //! Sets named parameter for the renderer.
+  SetParam: (context: ContextType, name: string, value: number) => ErrorType
 
-// RayOffsetFactor
-// MaxRadiance
-// RngCoherency
-// ToApplyHdrBloom
-// BloomThreshold
-// BloomSpreading
-// BloomSoftening
-// UseNearestFiltering
-// ToHideLights
-// Contrast
-// ContrastMidpoint
-// MaxBouncesTotal
-// MaxBouncesDiffuse
-// MaxBouncesGlossy
-// Saturation
-// Exposure
+  //! Returns named parameter of the renderer.
+  GetParam: (context: ContextType, name: string) => number
 
-//! Sets local node transformation matrix.
-//! Final (world) transformation may be affected by parent node.
-SetNodeLocalTransform : (context: ContextType, node: NodeType, matrix: number[]) => ErrorType;
+  // List of possible parameters:
 
-//! Sets world node transformation matrix.
-//! Local transformation will be calculated to result in target world transformation.
-SetNodeWorldTransform : (context: ContextType, node: NodeType, matrix: number[]) => ErrorType;
+  // RayOffsetFactor
+  // MaxRadiance
+  // RngCoherency
+  // ToApplyHdrBloom
+  // BloomThreshold
+  // BloomSpreading
+  // BloomSoftening
+  // UseNearestFiltering
+  // ToHideLights
+  // Contrast
+  // ContrastMidpoint
+  // MaxBouncesTotal
+  // MaxBouncesDiffuse
+  // MaxBouncesGlossy
+  // Saturation
+  // Exposure
 
-//! Returns local node transformation matrix.
-GetNodeLocalTransform : (context: ContextType, node: NodeType) => number[];
+  //! Sets local node transformation matrix.
+  //! Final (world) transformation may be affected by parent node.
+  SetNodeLocalTransform : (context: ContextType, node: NodeType, matrix: number[]) => ErrorType;
 
-//! Returns world node transformation matrix.
-GetNodeWorldTransform : (context: ContextType, node: NodeType) => number[];
+  //! Sets world node transformation matrix.
+  //! Local transformation will be calculated to result in target world transformation.
+  SetNodeWorldTransform : (context: ContextType, node: NodeType, matrix: number[]) => ErrorType;
 
-//! Sets node material color.
-SetNodeMaterialColor: (context: ContextType, node: NodeType, property: MaterialPropertyColor, color: number[]) => ErrorType;
+  //! Returns local node transformation matrix.
+  GetNodeLocalTransform : (context: ContextType, node: NodeType) => number[];
 
-//! Sets node material property.
-SetNodeMaterialValue: (context: ContextType, node: NodeType, property: MaterialProperty, value: number) => ErrorType;
+  //! Returns world node transformation matrix.
+  GetNodeWorldTransform : (context: ContextType, node: NodeType) => number[];
 
-//! Returns root of scene hierarchy.
-GetSceneRootNode: (context: ContextType) => NodeType;
+  //! Sets node material color.
+  SetNodeMaterialColor: (context: ContextType, node: NodeType, property: MaterialPropertyColor, color: number[]) => ErrorType;
 
-//! Returens number of sub-nodes for given node.
-GetSubNodeCount: (context: ContextType, node: NodeType) => number;
+  //! Sets node material property.
+  SetNodeMaterialValue: (context: ContextType, node: NodeType, property: MaterialProperty, value: number) => ErrorType;
 
-//! Returns sub-node with give index.
-GetSubNode: (context: ContextType, node: NodeType, index: number) => NodeType;
+  //! Returns root of scene hierarchy.
+  GetSceneRootNode: (context: ContextType) => NodeType;
 
-//! Returns node name.
-GetNodeName: (context: ContextType, node: NodeType) => string;
+  //! Returens number of sub-nodes for given node.
+  GetSubNodeCount: (context: ContextType, node: NodeType) => number;
 
-//! Sets node name.
-SetNodeName: (context: ContextType, node: NodeType, name: string) => ErrorType;
+  //! Returns sub-node with give index.
+  GetSubNode: (context: ContextType, node: NodeType, index: number) => NodeType;
 
-//! Sets node visibility mode.
-SetNodeVisible: (context: ContextType, node: NodeType, visibilityMode: VisibilityMode) => ErrorType;
+  //! Returns node name.
+  GetNodeName: (context: ContextType, node: NodeType) => string;
 
-//! Sets texture with a given role for node material.
-SetNodeTexture: (context: ContextType, node: NodeType, texture: TextureType, role: TextureRole) => ErrorType;
+  //! Sets node name.
+  SetNodeName: (context: ContextType, node: NodeType, name: string) => ErrorType;
+
+  //! Sets node visibility mode.
+  SetNodeVisible: (context: ContextType, node: NodeType, visibilityMode: VisibilityMode) => ErrorType;
+
+  //! Sets texture with a given role for node material.
+  SetNodeTexture: (context: ContextType, node: NodeType, texture: TextureType, role: TextureRole) => ErrorType;
+
+  //! Remove object from 3D scene.
+  DeleteNode: (context: ContextType, node: NodeType) => ErrorType;
+
+  //! Makes new environment map light source.
+  AddEnvironmentLight: (context: ContextType) => ErrorType;
+  
+  //! Removes light source from environment map.
+  DeleteEnvironmentLight: (context: ContextType, light: LightEnvType) => ErrorType;
+  
+  //! Returns whether light is visible.
+  GetEnvironmentLightVisible: (context: ContextType, light: LightEnvType) => boolean;
+  
+  //! Sets light visibility.
+  SetEnvironmentLightVisible: (context: ContextType, light: LightEnvType, isVisible: boolean) => ErrorType;
+
+  //! Sets environment light direction.
+  SetEnvironmentLightDirection: (context: ContextType, light: LightEnvType, direction: number[]) => ErrorType;
+
+  //! Sets environment light color.
+  SetEnvironmentLightColor: (context: ContextType, light: LightEnvType, color: number[]) => ErrorType;
+  
+  //! Sets named parameter for given light source.
+  SetEnvironmentLightParam: (context: ContextType, light: LightEnvType, name: string, value: number) => ErrorType;
+  
+  //! Returns named parameter value for given light source.
+  GetEnvironmentLightParam: (context: ContextType, light: LightEnvType, name: string) => number;
+  
+  //! Returns number of animation clips associated with the node.
+  GetNodeClipCount: (context: ContextType, node: NodeType) => number;
+  
+  //! Gets animation clip associated with the node by index.
+  GetNodeClip: (context: ContextType, node: NodeType, index: number) => ClipType;
+  
+  //! Evaluates animation for the given time point.
+  SetClipAnimationTime: (context: ContextType, node: NodeType, clip: ClipType, animationTime: number) => ErrorType;
+  
+  //! Returns clip name.
+  GetClipName: (context: ContextType, clip: ClipType) => string;
+}
+
+declare class View {
+  setViewport: (left: number, top: number, width: number, height: number) => void;
+  setCameraMatrices(viewMatrix: number[], projMatrix: number[]);
+}
+
+//! Helper class to move memory JS <=> WASM
+declare class BufferDescriptor {}
+
+declare class Renderer {
+  constructor(canvasSelector: string, appToken: string);
+
+  //! Returns context handle required by most API calls accessible through API object.
+  getContextHandle: () => ContextType;
+
+  //! Loads environment map from buffer and sets it for immediate use by renderer.
+  loadEnvironmentMapFromBuffer: (buffer: BufferDescriptor) => void;
+
+  //! Decodes the texture provided with a file buffer and returns texture handle.
+  loadTextureFromBuffer: (id: string, buffer: BufferDescriptor) => TextureType;
+
+  //! Loads GLTF scene from buffer and returns root node handle.
+  loadSceneFromBuffer: (buffer: BufferDescriptor) => NodeType;
+
+  //! Forces engine to reset frame accumulation and start rendering from scratch.
+  //! Usually it is not needed to call this since the engine tracks changes of data and settings.
+  resetFrameAccumulation: () => void;
+
+  //! Tells the engine whether float texture interpolation WebGL2 extension is supported.
+  setFloatTextureInterpolationSupport: (isSupported: boolean) => void;
+
+  //! Renders consequent path tracing sample and displays currently accumulated HDR image with tone-mapping and other post-processing.
+  draw: (view: View) => void;
+
+  //! Creates view object
+  createView: () => View;
+
+  //! Destroys view object
+  destroyView: (view: View) => void;
+}
---
+++ three-minimal/Camera.js
@@ -0,0 +1,145 @@
+import { Matrix4 } from './Matrix4.js';
+import { Vector3 } from './Vector3.js';
+import { Quaternion } from './Quaternion.js';
+import { Euler } from './Euler.js';
+
+const _v1 = /*@__PURE__*/ new Vector3();
+const _q1 = /*@__PURE__*/ new Quaternion();
+const _m1 = /*@__PURE__*/ new Matrix4();
+const _target = /*@__PURE__*/ new Vector3();
+
+const _position = /*@__PURE__*/ new Vector3();
+
+class Camera {
+
+	constructor() {
+
+		this.position = new Vector3();
+		this.rotation = new Euler();
+		this.quaternion = new Quaternion();
+		this.scale = new Vector3( 1, 1, 1 );
+		this.up = new Vector3( 0, 1, 0 );
+
+
+    this.modelViewMatrix = new Matrix4();
+
+		this.matrix = new Matrix4();
+		this.matrixWorld = new Matrix4();
+
+		this.isCamera = true;
+
+		this.type = 'Camera';
+
+		this.matrixWorldInverse = new Matrix4();
+
+		this.projectionMatrix = new Matrix4();
+		this.projectionMatrixInverse = new Matrix4();
+
+	}
+
+	copy( source, recursive ) {
+
+		this.up.copy( source.up );
+
+		this.position.copy( source.position );
+		this.rotation.order = source.rotation.order;
+		this.quaternion.copy( source.quaternion );
+		this.scale.copy( source.scale );
+
+		this.matrix.copy( source.matrix );
+		this.matrixWorld.copy( source.matrixWorld );
+
+		this.matrixWorldInverse.copy( source.matrixWorldInverse );
+
+		this.projectionMatrix.copy( source.projectionMatrix );
+		this.projectionMatrixInverse.copy( source.projectionMatrixInverse );
+
+		return this;
+
+	}
+
+  lookAt( x, y, z ) {
+
+		// This method does not support objects having non-uniformly-scaled parent(s)
+
+		if ( x.isVector3 ) {
+
+			_target.copy( x );
+
+		} else {
+
+			_target.set( x, y, z );
+
+		}
+
+		const parent = this.parent;
+
+		this.updateWorldMatrix( true, false );
+
+		_position.setFromMatrixPosition( this.matrixWorld );
+
+		if ( this.isCamera || this.isLight ) {
+
+			_m1.lookAt( _position, _target, this.up );
+
+		} else {
+
+			_m1.lookAt( _target, _position, this.up );
+
+		}
+
+		this.quaternion.setFromRotationMatrix( _m1 );
+
+		if ( parent ) {
+
+			_m1.extractRotation( parent.matrixWorld );
+			_q1.setFromRotationMatrix( _m1 );
+			this.quaternion.premultiply( _q1.invert() );
+
+		}
+
+	}
+
+	getWorldDirection( target ) {
+
+		this.updateWorldMatrix( true, false );
+
+		const e = this.matrixWorld.elements;
+
+		return target.set( - e[ 8 ], - e[ 9 ], - e[ 10 ] ).normalize();
+
+	}
+
+  updateMatrix() {
+		this.matrix.compose( this.position, this.quaternion, this.scale );
+  }
+
+	updateMatrixWorld( force ) {
+
+		this.updateMatrix();
+
+    this.matrixWorld.copy( this.matrix );
+
+		this.matrixWorldInverse.copy( this.matrixWorld ).invert();
+
+	}
+
+	updateWorldMatrix( updateParents, updateChildren ) {
+
+		this.updateMatrix();
+
+		this.matrixWorld.copy( this.matrix );
+
+		this.matrixWorldInverse.copy( this.matrixWorld ).invert();
+
+	}
+
+	clone() {
+
+		return new this.constructor().copy( this );
+
+	}
+
+}
+
+export { Camera };
